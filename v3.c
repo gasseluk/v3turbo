46ab196e3cdf15aea9cd08ec56983bff7d2b8281
looze <gasserlu@gmail.com>
2021-03-24 15:15:18 +0100

    Set the powerlimits - PKG Power limit - IMON slope/offset - ICCMAX -> locked ->BIOS mod necessary - MCHBAR -> locked, no effect

--- v3.c
+++ v3.c
@@ -3,16 +3,20 @@
 //###############################
 
 // Core Options
-#define CoreVOffset   -70 /* mV */
+#define CoreVOffset   -60 /* mV */
 #define CoreVStatic   0   /* mV */
 #define CoreMulti     0
 #define CoreFixed     0
 
 // Cache Options
-#define CacheVOffset  -50
+#define CacheVOffset  0
 #define CacheVStatic  0
 #define CacheMulti    0
 #define CacheFixed    0
+
+// IMON Options
+#define ImonSlope 0x001ULL
+#define ImonOffset 0x80ULL
 
 
 
@@ -74,8 +78,29 @@ static inline void wrmsr(UINT32 msr, UINT64 value)
 #define        MSR_TURBO_RATIO_LIMIT      0x1AD
 #define        MSR_TURBO_RATIO_LIMIT1     0x1AE
 #define        MSR_TURBO_RATIO_LIMIT2     0x1AF
-#define        MSR_TURBO_POWER_LIMIT      0x610  
+#define        MSR_VR_CURRENT_CONFIG      0x601
+#define        MSR_VR_MISC_CONFIG         0x603
+#define        MSR_PKG_POWER_SKU_UNIT     0x606
+#define        MSR_PKG_POWER_LIMIT        0x610  
+#define        MSR_PKG_POWER_SKU          0x614
+#define        MSR_DDR_RAPL_LIMIT         0x618
 #define        MSR_UNCORE_RATIO_LIMIT     0x620
+#define        MSR_VR_MISC_CONFIG2        0x636
+#define        MSR_PP0_POWER_LIMIT        0x638
+#define        MSR_PP1_POWER_LIMIT        0x640
+
+// BARs
+#define DEFAULT_MCHBAR		0xfed10000	/* 16 KB */
+#define MCHBAR32(x) *((volatile UINT32 *)(DEFAULT_MCHBAR + x))
+#define MCHBAR32_OR(x, or) MCHBAR32(x) = (MCHBAR32(x) | (or))
+/* Some power MSRs are also represented in MCHBAR */
+#define MCH_PKG_POWER_LIMIT_LO	0x59a0
+#define MCH_PKG_POWER_LIMIT_HI	0x59a4
+
+#define MCH_PL_LO 0xfed159a0
+#define MCH_PL_HI 0xfed159aa
+
+#define MCH_PL_VAL(X) *((volatile UINT32 *)(X))
 
 // Some Constants
 #define        CPUID_SIGNATURE                    0x306F2               // target CPUID, set 0xFFFFFFFF to bypass checking
@@ -83,6 +108,11 @@ static inline void wrmsr(UINT32 msr, UINT64 value)
 #define        MSR_FLEX_RATIO_OC_LOCK_BIT         (1<<20)               // bit 20, set to lock MSR 0x194
 #define        MSR_TURBO_RATIO_SEMAPHORE_BIT      0x8000000000000000    // set to execute changes writen to MSR 0x1AD, 0x1AE, 0x1AF
 #define        OC_MB_FIVR_DYN_SVID_CONTROL_DIS    0x80000000			      // bit 31
+#define        PLATFORM_INFO_SET_TDP              (1ULL << 29)
+#define        PKG_POWER_LIMIT_MASK               (0x7fffULL)
+#define        PKG_POWER_LIMIT_EN                 (1ULL << 15)
+#define        PKG_POWER_LIMIT_TIME_SHIFT         17
+#define        PKG_POWER_LIMIT_TIME_MASK          (0x7fULL)
 
 // Some macros for OC MB voltage calculation
 #define AdjVOffset(V) ((unsigned)((V << 15)/1000) & 0x000000000000ffe0)
@@ -107,6 +137,10 @@ typedef struct {
   UINT64 core;
   UINT64 cache;
 } cpu_caps_t;
+
+
+// Forward declarations
+void set_power_limits();
 
 
 EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SysTab) {
@@ -174,7 +208,18 @@ EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SysTab) {
     val |= limit | (limit << 8);
     wrmsr(MSR_UNCORE_RATIO_LIMIT, val);
   }
-
+/*
+  // Set dunno
+  {
+    UINT64 val = caps.core & 0xff;
+    val |= (val << 8) | (val << 16) | (val << 24);
+    val |= (0x03ULL << 32);
+    OC_MB_CMD(val, OC_MB_DOMAIN_IACORE, rsp)
+    if ( OC_MB_ERROR(rsp) ){
+      Print(L"FAIL: Could not set dunno\r\n");
+    }
+  }
+*/
   // Set Core voltage
   {
     UINT64 val = CoreVoltage
@@ -207,6 +252,23 @@ EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SysTab) {
     }
   }
 
+  // IOUT_SLOPE / OFFSET
+  {
+    UINT64 val = rdmsr(MSR_VR_MISC_CONFIG);
+    val &= 0xfffc0000ffffffffULL;
+    val |= (ImonSlope << 40) | (ImonOffset << 32);
+    wrmsr(MSR_VR_MISC_CONFIG, val);
+  }
+
+  // IMAX
+  {
+    UINT64 val = rdmsr(MSR_VR_CURRENT_CONFIG);
+    val |= 0x0000000000001fff;
+    wrmsr(MSR_VR_CURRENT_CONFIG, val);
+  }
+
+  set_power_limits();
+
   // Lock MSR_FLEX_RATIO using the FLEX_RATIO_LOCK bit
   // This is needed to lock down our settings, otherwise
   // they would be corrected by the microcode update in the OS
@@ -218,4 +280,51 @@ EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SysTab) {
 
 exit:
   return EFI_SUCCESS;
+}
+
+
+/*
+ * Configure processor power limits if possible
+ * This must be done AFTER set of BIOS_RESET_CPL
+ */
+void set_power_limits()
+{
+	UINT64 msr = rdmsr(MSR_PLATFORM_INFO);
+	UINT64 limit;
+	unsigned int power_unit;
+	unsigned int tdp, min_power, max_power, max_time;
+
+	if (!(msr & PLATFORM_INFO_SET_TDP))
+		return;
+
+	/* Get units */
+	msr = rdmsr(MSR_PKG_POWER_SKU_UNIT);
+	power_unit = 2 << ((msr & 0xf) - 1);
+
+	/* Get power defaults for this SKU */
+	msr = rdmsr(MSR_PKG_POWER_SKU);
+	tdp = msr & 0x7fff;
+	min_power = (msr >> 16) & 0x7fff;
+	max_power = (msr >> 32)  & 0x7fff;
+	max_time = (msr >> 48) & 0x7f;
+
+	Print(L"CPU TDP: %u Watts\r\n", tdp / power_unit);
+
+	/* Set long term power limit to TDP */
+	limit = 0;
+	limit |= PKG_POWER_LIMIT_MASK;
+	limit |= PKG_POWER_LIMIT_EN;
+	limit |= PKG_POWER_LIMIT_TIME_MASK <<	PKG_POWER_LIMIT_TIME_SHIFT;
+
+	/* Set short term power limit to 1.25 * TDP */
+	limit |= (PKG_POWER_LIMIT_MASK << 32);
+	limit |= (PKG_POWER_LIMIT_EN << 32);
+	/* Power limit 2 time is only programmable on server SKU */
+
+	wrmsr(MSR_PKG_POWER_LIMIT, limit);
+
+	/* Set power limit values in MCHBAR as well */
+	MCH_PL_VAL(MCH_PL_LO) = limit & 0x0ffffffff;
+	MCH_PL_VAL(MCH_PL_HI) = (limit >> 32) & 0x0ffffffff;
+
 }
